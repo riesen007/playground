794a3a569fb6dad39ad0f9567dc9fa39f2a4e6fc
riesen007 <riesen@yahoo.de>
2016-11-17 11:20:31 +0100

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,45 +1,22 @@
-$heroku create myapp --buildpack heroku/nodejs
 'use strict';
+angular.module('tutorialApp', [ 'ngAnimate', 'ngRoute' ]).config(
+		function($routeProvider) {
+			$routeProvider.when('/linksAxa', {
+				templateUrl : 'linksAxa.html'
+			}).when('/linksPrivat', {
+				templateUrl : 'linksPrivat.html'
+			}).when('/start', {
+				template : 'Hier steht noch nichts'
+			}).otherwise({
+				redirectTo : '/'
+			});
+		}).controller('LinksPrivatCtrl', function($scope, $http) {
+	$http.get('linksPrivat.json').then(function(linksPrivatResponse) {
+		$scope.linksPrivat = linksPrivatResponse.data;
+	});
 
-angular.module('tutorialApp', ['ngAnimate', 'ngRoute'])
-  .config(function($routeProvider) {
-    $routeProvider
-      .when('/', { templateUrl: 'articles.html' })
-      .when('/about', { template: 'Über unsere Pizzeria' })
-      .otherwise({ redirectTo: '/'});
-  })
-  .directive('price', function(){
-    return {
-      restrict: 'E',
-      scope: {
-        value: '='
-      },
-      template: '<span ng-show="value == 0">kostenlos</span>' +
-        '<span ng-show="value > 0">{{value | currency}}</span>'
-    }
-  })
-  .factory('Cart', function() {
-    var items = [];
-    return {
-      getItems: function() {
-        return items;
-      },
-      addArticle: function(article) {
-        items.push(article);
-      },
-      sum: function() {
-        return items.reduce(function(total, article) {
-          return total + article.price;
-        }, 0);
-      }
-    };
-  })
-  .controller('ArticlesCtrl', function($scope, $http, Cart){
-    $scope.cart = Cart;
-    $http.get('articles.json').then(function(articlesResponse) {
-      $scope.articles = articlesResponse.data;
-    });
-  })
-  .controller('CartCtrl', function($scope, Cart){
-    $scope.cart = Cart;
-  });
+}).controller('LinksAxaCtrl', function($scope, $http) {
+		$http.get('linksAxa.json').then(function(linksAxaResponse) {
+			$scope.linksAxa = linksAxaResponse.data;
+		});
+});
